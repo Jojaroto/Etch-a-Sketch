2bb079c4dc5bd06505269aa5e90ef0c9f1b688c9
George Fernandes <paodequeijooj@outlook.com>
2022-03-24 12:18:36 -0300

    etch a sketch finalized

--- app.js
+++ app.js
@@ -1,27 +1,57 @@
-let rows = document.getElementsByClassName("gridRow")
-let cells = document.getElementsByClassName("cell")
+const container = document.querySelector('#container');
+let cell;
+let target;
 
-
-function makeRows(rowNum){
-    const container = document.getElementById("container")
-    console.log(container)
-    for (r= 0; r < rowNum; r++){
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    };
-};
-
-function makeColumns(cellNum){
-    for (i = 0; i < rows.length; i++){
-        for (j= 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        }
+//criar celulas
+function makeCell (numCell) {
+    for (let c = 0; c < numCell; c++ ) {
+        cell = document.createElement('div');
+        cell.classList.add('cell');
+        container.appendChild(cell);
     }
 }
 
-function defaultGrid (){
-    makeRows(16);
-    makeColumns(16);
+//celulas em cada coluna
+
+function makeGrid (numRow, numCol) {
+    container.style.setProperty('--numRows', numRow);
+    container.style.setProperty('--numCols', numCol);
+    for (let r = 0; r < numRow; r++) {
+        makeCell(numCol)
+    }
 }
 
+function promptGrid() {
+    let number = prompt("pick a size between 5-64.", 16);
+    if (number >= 5 && number <= 64) {
+        makeGrid (number,number);
+    }else{
+        do{
+            number = prompt("Invalid size.");
+        }
+        while(number < 5 || number> 64);
+        makeGrid(number,number)
+    }
+}
+
+//mouseover activation
+
+function changeColor(target) {
+    target.style.backgroundColor = 'black';
+}
+
+container.addEventListener("mouseover", function(e) {
+    target = e.target;
+
+    if (target.matches("div.cell")) {
+        changeColor(target);
+    }
+})
+
+//reset button
+const reset = document.querySelector('.resetBtn');
+reset.addEventListener('click', function(){
+    window.location.reload();
+})
+
+promptGrid()
